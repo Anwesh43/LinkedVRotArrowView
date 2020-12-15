92f3dbda9d4443ac30ea689a742e253c67808b4e
Anwesh43 <anweshthecool0@gmail.com>
2020-12-16 03:16:55 +0530

    adding Int and Float extension methods

--- vrotarrowview/src/main/java/com/example/vrotarrowview/VRotArrowView.kt
+++ vrotarrowview/src/main/java/com/example/vrotarrowview/VRotArrowView.kt
@@ -25,3 +25,8 @@ val scGap : Float = 0.02f / parts
 val rot : Float = 45f
 val arrowFactor : Float = 12.3f
 val delay : Long = 20
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
