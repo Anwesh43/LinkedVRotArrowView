c04a45b1b845dfe12044437d8ed6bfae43ef0758
Anwesh43 <anweshthecool0@gmail.com>
2020-12-16 03:48:51 +0530

    adding VRANode class

--- vrotarrowview/src/main/java/com/example/vrotarrowview/VRotArrowView.kt
+++ vrotarrowview/src/main/java/com/example/vrotarrowview/VRotArrowView.kt
@@ -127,4 +127,45 @@ class VRotArrowView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class VRANode(var i : Int, val state : State = State()) {
+
+        private var next : VRANode? = null
+        private var prev : VRANode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = VRANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawVRANode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : VRANode {
+            var curr : VRANode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
